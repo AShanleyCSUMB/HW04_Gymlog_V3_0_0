692ef547f627e183d6a9acad850b112c27f8fd2f
Aidan Shanley <ashanley@csumb.edu>
2025-11-02 20:33:47 -0800

    Update MainActivity.java hooray!

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,20 +1,221 @@
-package com.example.hw04_gymlog_v300;
+package com.example.gymlog;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.gymlog.database.GymLogRepository;
+import com.example.gymlog.database.entities.GymLog;
+import com.example.gymlog.database.entities.User;
+import com.example.gymlog.databinding.ActivityMainBinding;
+import com.example.gymlog.viewHolders.GymLogAdapter;
+import com.example.gymlog.viewHolders.GymLogViewModel;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_USER_ID = "MAIN_ACTIVITY_USER_ID";
+    static final String SHARED_PREFERENCE_USERID_KEY = "SHARED_PREFERENCE_USERID_KEY";
+    static final String SAVED_INSTANCE_STATE_USERID_KEY = "SHARED_PREFERENCE_USERID_KEY";
+    private static final int LOGGED_OUT = -1;
+
+    private ActivityMainBinding binding;
+    private GymLogRepository repository;
+    private GymLogViewModel gymLogViewModel;
+    public static final String TAG = "GYMLOG";
+    String mExercise = "";
+    double mWeight = 0.0;
+    int mReps = 0;
+
+    private int loggedInUserId = -1;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        gymLogViewModel = new ViewModelProvider(this).get(GymLogViewModel.class);
+        RecyclerView recyclerView = binding.logDisplayRecyclerView;
+        final GymLogAdapter adapter = new GymLogAdapter(new GymLogAdapter.GymLogDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        repository = GymLogRepository.getRepository(getApplication());
+        loginUser(savedInstanceState);
+        gymLogViewModel.getAllLogsById(loggedInUserId).observe(this, gymLogs -> {
+            adapter.submitList(gymLogs);
+        });
+        //User not logged in, go to login screen
+        if(loggedInUserId == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+        updateSharedPreference();
+        
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+                insertGymLogRecord();
+                
+            }
+        });
 
-        setContentView(R.layout.activity_main);
+    }
 
+    private void loginUser(Bundle savedInstanceState) {
+
+        //check shared preference for logged in users
+        
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        loggedInUserId = sharedPreferences.getInt(getString(R.string.preference_file_key), LOGGED_OUT);
+
+        if(loggedInUserId == LOGGED_OUT & savedInstanceState != null && savedInstanceState.containsKey(SAVED_INSTANCE_STATE_USERID_KEY)){
+            loggedInUserId = savedInstanceState.getInt(SAVED_INSTANCE_STATE_USERID_KEY, LOGGED_OUT);
+        }
+        if(loggedInUserId == LOGGED_OUT){
+            loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        }
+        if(loggedInUserId == LOGGED_OUT){
+            return;
+        }
+        LiveData<User> userObserver = repository.getUserByUserId(loggedInUserId);
+        userObserver.observe(this, user -> {
+            this.user = user;
+            if(user != null){
+                invalidateOptionsMenu();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVED_INSTANCE_STATE_USERID_KEY, loggedInUserId);
+        updateSharedPreference();
+    }
+
+    private void updateSharedPreference() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt(SHARED_PREFERENCE_USERID_KEY,LOGGED_OUT);
+        sharedPrefEditor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        item.setVisible(true);
+        if(user == null){
+            return false;
+        }
+        item.setTitle(user.getUsername());
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+        alertBuilder.setMessage("Logout?");
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        loggedInUserId = LOGGED_OUT;
+        updateSharedPreference();
+
+        getIntent().putExtra(MAIN_ACTIVITY_USER_ID, loggedInUserId);
+
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+
+    private void insertGymLogRecord (){
+        if(mExercise.isEmpty()){
+            return;
+        }
+
+        GymLog log = new GymLog(mExercise, mWeight, mReps, loggedInUserId);
+        repository.insertGymLog(log);
+    }
+
+    @Deprecated
+    private void updateDisplay(){
+        ArrayList<GymLog> allLogs = repository.getAllLogsByUserId(loggedInUserId);
+        if(allLogs.isEmpty()){
+            
+        }
+        StringBuilder sb = new StringBuilder();
+        for(GymLog log : allLogs){
+            sb.append(log);
+        }
+        
+    }
+
+    private void getInformationFromDisplay() {
+        mExercise = binding.exerciseInputText.getText().toString();
+        try{
+        mWeight = Double.parseDouble(binding.weightInputText.getText().toString());
+    } catch(NumberFormatException e){
+            Log.d(TAG, "Error reading value from weightInputText");
+        }
+        try {
+            mReps = Integer.parseInt(binding.repInputText.getText().toString());
+        } catch(NumberFormatException e){
+            Log.d(TAG, "Error reading value from repInputText");
+        }
     }
 }
